ac8825cf6f7c66753bb501746e0baec3416c62e2
Mikołaj Szawerda <96917430+MikolajSzawerda@users.noreply.github.com>
2023-03-10 00:35:45 +0100

    fix docs

--- src/scrapers/model_plan_scraper.py
+++ src/scrapers/model_plan_scraper.py
@@ -16,7 +16,7 @@ def scrape_model_plan(text, course, semester: int):
     :param text: html text of website
     :param course: course name
     :param semester: semester number
-    :return: generator of subject urls
+    :return: list of subject urls
     """
     subjects = BeautifulSoup(text, 'html.parser') \
         .find(attrs={'summary': COURSES[course]}) \
