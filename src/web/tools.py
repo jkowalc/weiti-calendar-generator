ba1ec8fc88e1581b8d0b19b7c6be68a76c9dbd3d
mszawerd <01169246@pw.edu.pl>
2023-03-10 00:32:11 +0100

    Added scraping plan model for given cource

--- src/web/tools.py
+++ src/web/tools.py
@@ -1,4 +1,22 @@
 import grequests
+import requests
+
+COURSES = {
+    'Informatyka': 'https://www.elka.pw.edu.pl/Studenci/Zalaczniki-i-formularze/Zalaczniki/Plany-modelowe-12/Informatyka-od-roku-2019-2020-1',
+    'Automatyka i robotyka': 'https://www.elka.pw.edu.pl/Studenci/Zalaczniki-i-formularze/Zalaczniki/Plany-modelowe-12/Automatyka-i-robotyka-1',
+    'Cyberbezpieczeństwo': 'https://www.elka.pw.edu.pl/Studenci/Zalaczniki-i-formularze/Zalaczniki/Plany-modelowe-12/Cyberbezpieczenstwo-1',
+    'Elektronika': 'https://www.elka.pw.edu.pl/Studenci/Zalaczniki-i-formularze/Zalaczniki/Plany-modelowe-12/Elektronika-do-roku-2019-2020-1',
+    'Telekomunikacja': 'https://www.elka.pw.edu.pl/Studenci/Zalaczniki-i-formularze/Zalaczniki/Plany-modelowe-12/Telekomunikacja-od-roku-2019-2020-1',
+    'Inżynieria internetu rzeczy': 'https://www.elka.pw.edu.pl/Studenci/Zalaczniki-i-formularze/Zalaczniki/Plany-modelowe-12/Inzynieria-internetu-rzeczy-1',
+}
+
+
+def map_single_url(url, func, *args, **kwargs):
+    response = requests.get(url)
+    text = response.text
+    if text is None:
+        raise Exception('Given url returns empty website!')
+    return func(text, *args, **kwargs)
 
 
 def map_multiple_urls(urls, func, *args, **kwargs):
@@ -15,4 +33,4 @@ def map_multiple_urls(urls, func, *args, **kwargs):
 
 
 def get_model_plan_url(course):
-    return 'https://google.pl'
+    return COURSES[course]
