172db0dec9ae2cc10185132f699550a1d80860bb
Jan Kowalczewski <official.jan.kk@gmail.com>
2023-03-09 20:46:22 +0100

    Loaders

--- src/model/model_plan.py
+++ src/model/model_plan.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass
 
+from src.model.calendar_semester import CalendarSemester
+
 
 @dataclass
 class ModelPlan:
     semester: int
     course: str
+    calendar_semester: CalendarSemester
     subjects: list
 
