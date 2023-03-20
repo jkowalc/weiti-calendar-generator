b57d518a3066ce7d3c401088b44f862a9cdd1aa0
Jan Kowalczewski <official.jan.kk@gmail.com>
2023-03-20 15:18:17 +0100

    First working version

--- src/dev/generate_topics.py
+++ src/dev/generate_topics.py
@@ -4,13 +4,23 @@ def generate_topics_for_subject(subject):
     for class_type in subject.classes_schedule:
         if len(subject.classes_schedule[class_type]) > 1:
             return_dict[class_type.name] = {}
-            for group in subject.classes_schedule[class_type]:
-                topic_count = len(subject.classes_schedule[class_type][group])
-                return_dict[class_type.name][group] = [[""] * topic_count]
+            counts = {
+                group: len(subject.classes_schedule[class_type][group])
+                for group in subject.classes_schedule[class_type]
+            }
+            same_count = all(count == counts[list(counts.keys())[0]] for count in counts.values())
+            if not same_count:
+                for group in subject.classes_schedule[class_type]:
+                    topic_count = len(subject.classes_schedule[class_type][group])
+                    return_dict[class_type.name][group] = [""] * topic_count
+            else:
+                key = list(counts.keys())[0]
+                topic_count = counts[key]
+                return_dict[class_type.name] = [""] * topic_count
         else:
             key = list(subject.classes_schedule[class_type].keys())[0]
             topic_count = len(subject.classes_schedule[class_type][key])
-            return_dict[class_type.name] = [[""] * topic_count]
+            return_dict[class_type.name] = [""] * topic_count
     return return_dict
 
 
--- src/main.py
+++ src/main.py
@@ -1,25 +1,43 @@
 from ics import Calendar
 
+from src.additional_info.filter_schedule import filter_schedules_for_subject
+from src.additional_info.generate_events import generate_events
+from src.additional_info.load_info import load_whitelists, load_topics
+from src.ui.select_group_choice import select_group_choice
+from src.ui.select_model_plan import select_model_plan
+from src.ui.select_new_subject import select_new_subject
+from src.ui.select_semester import select_calendar_semester
+from src.web.full_loader import load_model_plan
+
 
 def main():
-    # cal = Calendar()
-    # bss_group = ''
-    # print('Możliwości: 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, brak')
-    # while bss_group not in ['101', '102', '103', '104', '105', '106', '107', '108', '109', '110', 'brak']:
-    #     bss_group = input('Podaj grupę BSS: ')
-    # cal.events = cal.events.union(generate_events_bss(bss_group))
-    # wmm_group = ''
-    # print('Możliwości: 101, 102, 103, 104, 105, 106, 107, 108, brak')
-    # while wmm_group not in ['101', '102', '103', '104', '105', '106', '107', '108', 'brak']:
-    #     wmm_group = input('Podaj grupę WMM: ')
-    # cal.events = cal.events.union(generate_events_wmm(wmm_group))
-    # cal.events = cal.events.union(generate_events_wsyz())
-    # cal.events = cal.events.union(generate_events_swo())
-    # cal.events = cal.events.union(generate_events_skps())
-    # cal.events = cal.events.union(generate_events_bd2())
-    # with open('../out.ics', 'w') as fp:
-    #     fp.writelines(cal.serialize_iter())
+    cal = Calendar()
+    calendar_semester = select_calendar_semester()
+    whitelists = load_whitelists(calendar_semester)
+    topics = load_topics(calendar_semester)
+    inp = ""
+    while inp not in ['y', 'n']:
+        inp = input("Czy chcesz dodać plan modułowy? (y/n): ").strip()
+    if inp == 'y':
+        semester, course = select_model_plan()
+        model_plan = load_model_plan(semester, course, calendar_semester)
+        for subject in model_plan.subjects:
+            filter_schedules_for_subject(subject, whitelists)
+            user_choice = select_group_choice(subject)
+            cal.events = cal.events.union(generate_events(calendar_semester, subject, topics, user_choice))
+    while True:
+        inp = input("Czy chcesz dodać przedmiot? (y/n): ").strip()
+        if inp == 'n':
+            break
+        elif inp == 'y':
+            subject = select_new_subject()
+            filter_schedules_for_subject(subject, whitelists)
+            user_choice = select_group_choice(subject)
+            cal.events = cal.events.union(generate_events(calendar_semester, subject, topics, user_choice))
+    with open('../out.ics', 'w') as fp:
+        fp.writelines(cal.serialize_iter())
     pass
+
 
 if __name__ == '__main__':
     main()
