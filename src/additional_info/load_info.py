3d422e5e42516baf724a8aa13d89f8f0f3f32419
Jan Kowalczewski <official.jan.kk@gmail.com>
2023-03-20 13:01:42 +0100

    Generating events

--- src/additional_info/load_info.py
+++ src/additional_info/load_info.py
@@ -11,10 +11,12 @@ def load_thing(thing, semester: CalendarSemester):
         with open(f"../../config/{thing}/{file}", "r") as f:
             try:
                 things_as_json: dict = json.load(f)
-            except json.decoder.JSONDecodeError:
-                raise RuntimeError(f"File {file} is not a valid JSON file.")
+            except json.JSONDecodeError:
+                raise RuntimeError(f"File {file} is not a valid JSON file.") from None
             if things_as_json.get("semester") == semester.code:
                 things_as_json.pop("semester")
+                if set(things.keys()).intersection(set(things_as_json.keys())):
+                    raise RuntimeError(f"File {file} duplicates key from other file")
                 things = things | things_as_json
     return things
 
@@ -28,5 +30,5 @@ def load_topics(semester: CalendarSemester):
 
 
 if __name__ == '__main__':
-    print(load_whitelists(CalendarSemester("23L")))
-    print(load_topics(CalendarSemester("23L")))
+    load_whitelists(CalendarSemester("23L"))
+    # print(load_topics(CalendarSemester("23L")))
