d0d7dbb6cd71084a13c3479c3fac7f1a9c324d0f
Jan Kowalczewski <official.jan.kk@gmail.com>
2023-05-15 14:43:37 +0200

    Refactor user choice

--- src/ui/add_choice_subjects.py
+++ src/ui/add_choice_subjects.py
@@ -3,6 +3,7 @@ import re
 from src.additional_info.filter_schedule import filter_schedules_for_subject
 from src.additional_info.generate_events import generate_events
 from src.model.subject import SUBJECT_CODE_REGEX
+from src.model.user_choice import UserChoice
 from src.web.subject_loader import load_subject
 
 
@@ -13,11 +14,11 @@ def parse_subject_code(inp):
         raise RuntimeError('.user_choice file corrupted')
 
 
-def add_choice_subjects(cal, calendar_semester, whitelists, topics, choice_subjects):
-    for subject_code in choice_subjects:
+def add_choice_subjects(cal, calendar_semester, whitelists, topics, choice: UserChoice):
+    for subject_code in choice.subjects:
         url = parse_subject_code(subject_code)
         subject = load_subject(url, calendar_semester)
         filter_schedules_for_subject(subject, whitelists)
-        user_choice = choice_subjects[subject_code]
-        choice_subjects[subject.full_code] = user_choice
+        user_choice = choice.subjects[subject_code]
+        choice.add_subject(subject.full_code, user_choice)
         cal.events = cal.events.union(generate_events(calendar_semester, subject, topics, user_choice))
--- src/ui/save_choice.py
+++ src/ui/save_choice.py
@@ -2,30 +2,13 @@ import json
 from pathlib import Path
 
 from src.model.subject import ClassType
+from src.model.user_choice import UserChoice
 
 SAVE_PATH = Path(".user_choice")
 
 
-def serialize_choice(choice):
-    new_choice = {'semester': choice['semester'], 'subjects': {}}
-    for subj in choice['subjects']:
-        new_choice['subjects'][subj] = {}
-        for class_type in choice['subjects'][subj]:
-            new_choice['subjects'][subj][class_type.value] = choice['subjects'][subj][class_type]
-    return new_choice
-
-
-def deserialize_choice(choice):
-    new_choice = {'semester': choice['semester'], 'subjects': {}}
-    for subj in choice['subjects']:
-        new_choice['subjects'][subj] = {}
-        for class_type_str in choice['subjects'][subj]:
-            new_choice['subjects'][subj][ClassType(class_type_str)] = choice['subjects'][subj][class_type_str]
-    return new_choice
-
-
-def save_choice(choice):
-    choice = serialize_choice(choice)
+def save_choice(choice: UserChoice):
+    choice = choice.serialize()
     try:
         with open(SAVE_PATH, mode="w") as f:
             json.dump(choice, f)
@@ -33,7 +16,7 @@ def save_choice(choice):
         print("Save failed")
 
 
-def load_choice():
+def load_choice() -> UserChoice:
     try:
         with open(SAVE_PATH, mode="r") as f:
             print("Znaleziono zapisany ostatni wybór.")
@@ -41,10 +24,10 @@ def load_choice():
             while inp not in ['y', 'n']:
                 inp = input("Czy chcesz załadować? (y/n): ").strip()
             if inp == 'y':
-                return deserialize_choice(json.load(f))
+                return UserChoice.from_deserialized(json.load(f))
             else:
-                return {}
+                return UserChoice()
     except FileNotFoundError:
-        return {}
+        return UserChoice()
     except KeyError:
         raise RuntimeError('.user_choice file corrupted')
